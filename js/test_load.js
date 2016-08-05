827116a3e8b9a6ddb09885fc730490c78fb9d95d
John Steinmetz <john.steinmetz@bazaarvoice.com>
2016-08-05 11:27:51 -0500

    added js adn links to json files

--- js/test_load.js
+++ js/test_load.js
@@ -1,15 +1,15 @@
 $(function() {
 
 	// call the function as many times as you need to load charts. Passing the 3 necessary fields that we created in test_functions.js
-    buildChartSmall("#analytics1", "pie", "/training/data/analytics1.json");
-    buildChartSmall("#analytics2", "pie", "/training/data/analytics2.json");
-    buildChartSmall("#analytics3", "pie", "/training/data/analytics3.json");
-    buildChartSmall("#analytics4", "area-spline", "/training/data/areaChartSample.json");
+    buildChartSmall("#analytics1", "pie", "/data/analytics1.json");
+    buildChartSmall("#analytics2", "pie", "/data/analytics2.json");
+    buildChartSmall("#analytics3", "pie", "/data/analytics3.json");
+    buildChartSmall("#analytics4", "area-spline", "/data/areaChartSample.json");
 
-    buildChartCategories("#barchart", "bar", "/training/data/barChartCats.json");
+    buildChartCategories("#barchart", "bar", "/data/barChartCats.json");
 
 
-    buildChartSmall("#chart5", "area-spline", "/training/data/areaChartSample.json");
+    buildChartSmall("#chart5", "area-spline", "/data/areaChartSample.json");
 
 
     buildChartYear("#datedchart");
@@ -21,7 +21,7 @@ $(function() {
     buildChartSmallOther("#other2");
     buildChartSmallBV("#bv2");
 
-    buildChartCases("#casesPerMonth", "line", "/training/data/casesPerMonth.json")
+    buildChartCases("#casesPerMonth", "line", "/data/casesPerMonth.json")
     buildChartUniqueUsers("#uniqueUsers")
 
 });
